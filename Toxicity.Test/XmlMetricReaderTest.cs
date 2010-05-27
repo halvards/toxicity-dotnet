f25cfdf73d682d5f22f8c7af716a7906904312b2
Halvard Skogsrud <halvards@gmail.com>
2010-05-27 17:53:09 +0100

    Initial commit

---
+++ Toxicity.Test/XmlMetricReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Toxicity.Test
+{
+    [TestFixture]
+    public class XmlMetricReaderTest
+    {
+        [Test]
+        public void ShouldIgnoreTestClass()
+        {
+            Assert.IsTrue(XmlMetricReader.IgnoreType("XMLTest"));
+        }
+
+        [Test]
+        public void ShouldIgnoreModuleClasses()
+        {
+            Assert.IsTrue(XmlMetricReader.IgnoreType("<Module>"));
+        }
+
+        [Test]
+        public void ShouldNotIgnoreRegularClassName()
+        {
+            Assert.IsFalse(XmlMetricReader.IgnoreType("RegularClassName"));
+        }
+
+        [Test]
+        public void ShouldNotIgnoreEmptyString()
+        {
+            Assert.IsFalse(XmlMetricReader.IgnoreType(""));
+        }
+    }
+}
